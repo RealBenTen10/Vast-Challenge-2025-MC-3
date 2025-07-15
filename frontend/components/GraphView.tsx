ac6a94d20c03f49be6cb5962244fc60e4ded4d2b
benten10 <dabenedi@hu-berlin.de>
2025-07-15 12:53:05 +0200

    re-added Event click functionality

--- frontend/components/GraphView.tsx
+++ frontend/components/GraphView.tsx
@@ -989,6 +989,9 @@ const GraphView: React.FC<Props> = ({
             .on("mouseover", (event, d) => d3.select(event.currentTarget).select("circle").attr("stroke", "purple").attr("stroke-width", 4))
             .on("mouseout", (event, d) => d3.select(event.currentTarget).select("circle").attr("stroke", "none"))
             .on("click", (event: any, d: any) => {
+              if (d.type === "Event" && d.sub_type !== "Communication") {
+                setSelectedEventId(d.id);
+              }
               
               if (d.type === "Entity") 
               {
