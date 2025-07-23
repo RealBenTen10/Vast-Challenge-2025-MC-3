0902ce0f1f1d6783a3caacf10ab86abac1298bac
benten10 <dabenedi@hu-berlin.de>
2025-07-23 14:47:03 +0200

    Re-integrated missing elements which were lost during merge

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -19,6 +19,7 @@ import GraphView from "@/components/GraphView";
 import TimeBarChart from "@/components/TimeBarChart";
 
 export default function Home() {
+  const [statusMsg, setStatusMsg] = useState<string>("");
   const [graphData, setGraphData] = useState<GraphData>({ nodes: [], links: [] });
   const [commGraphData, setCommGraphData] = useState<GraphData>({ nodes: [], links: [] });
   const [useAggregated, setUseAggregated] = useState<boolean>(false);
